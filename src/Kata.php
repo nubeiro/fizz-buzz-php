570281c5fe7a4dfc24abf47abbaff7b40c76f0f9
Raúl Araya <nubeiro@gmail.com>
2018-04-23 22:36:40 +0200

    Refactor array_map to helper function.

--- src/Kata.php
+++ src/Kata.php
@@ -8,19 +8,25 @@ class Kata
 
     public function run(array $set = [])
     {
-        $set = array_map(function ($number) {
+        $ruleOfThree = function ($number) {
             if ($number % 3 === 0) {
                 return 'Fizz';
             }
             return $number;
-        },
-            $set);
-        $set = array_map(function($number) {
+        };
+        $ruleOfFive = function ($number) {
             if (is_int($number) && $number % 5 === 0) {
                 return 'Buzz';
             }
             return $number;
-        }, $set);
+        };
+        $set = $this->mapWith($ruleOfThree, $set);
+        $set = $this->mapWith($ruleOfFive, $set);
+
         return $set;
+    }
+
+    public function mapWith(callable $rule, $set) {
+        return array_map($rule, $set);
     }
 }
