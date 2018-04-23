3d4c51f96eb1395410805428e5d3cbc75b7ec69f
Raúl Araya <nubeiro@gmail.com>
2018-04-23 23:24:52 +0200

    Add failing test for last rule.

--- tests/KataTest.php
+++ tests/KataTest.php
@@ -11,20 +11,25 @@ class KataTest extends TestCase
     /**
      * @dataProvider fizzBuzzSets
      */
-    public function testMultiplesOfThreeAsFizz($set, $expected)
+    public function testFizzBuzz($set, $expected)
     {
         $this->assertEquals($expected, (new Kata())->run($set));
     }
 
-    public function fizzBuzzSets() {
+    public function fizzBuzzSets()
+    {
         return [
             'rules for three' => [
                 'set' => [3, 9, 12, 16, 21, 4],
                 'expected' => ['Fizz', 'Fizz', 'Fizz', 16, 'Fizz', 4],
             ],
             'rules for five' => [
-                'set' => [2, 4, 3, 9, 14, 15, 16, 20],
-                'expected' => [2, 4, 'Fizz', 'Fizz', 14, 'Fizz', 16, 'Buzz'],
+                'set' => [2, 4, 3, 9, 14, 16, 20],
+                'expected' => [2, 4, 'Fizz', 'Fizz', 14, 16, 'Buzz'],
+            ],
+            'rules for five and three' => [
+                'set' => [2, 3, 5, 15, 20, 25, 30],
+                'expected' => [2, 'Fizz', 'Buzz', 'FizzBuzz', 'Buzz', 'Buzz', 'FizzBuzz'],
             ]
         ];
     }
