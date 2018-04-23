d6c579d8c47fbecccb91413a0fd252ee6d54bac5
Raúl Araya <nubeiro@gmail.com>
2018-04-23 22:28:57 +0200

    Remove first test, add test for Fizz and Buzz as separate rules (first and second rule).

--- src/Kata.php
+++ src/Kata.php
@@ -6,8 +6,21 @@ namespace Kata;
 class Kata
 {
 
-    public function run()
+    public function run(array $set = [])
     {
-        echo implode("\n", range(0, 100));
+        $set = array_map(function ($number) {
+            if ($number % 3 === 0) {
+                return 'Fizz';
+            }
+            return $number;
+        },
+            $set);
+        $set = array_map(function($number) {
+            if (is_int($number) && $number % 5 === 0) {
+                return 'Buzz';
+            }
+            return $number;
+        }, $set);
+        return $set;
     }
 }
--- tests/KataTest.php
+++ tests/KataTest.php
@@ -8,10 +8,19 @@ use PHPUnit\Framework\TestCase;
 class KataTest extends TestCase
 {
 
-    public function testPrintOneLineFromRange()
+    public function testMultiplesOfThreeAsFizz()
     {
-        $this->expectOutputString(implode("\n", range(0, 100)));
-        $kata = new Kata();
-        $kata->run();
+        $set = [3, 9, 12, 16, 21, 4];
+        $expected = ['Fizz', 'Fizz', 'Fizz', 16, 'Fizz', 4];
+        $this->assertEquals($expected, (new Kata())->run($set));
     }
+
+    public function testMultiplesOfFiveAsBuzz()
+    {
+        $set = [2, 4, 3, 9, 14, 15, 16, 20];
+        $expected = [2, 4, 'Fizz', 'Fizz', 14, 'Fizz', 16, 'Buzz'];
+        $this->assertEquals($expected, (new Kata())->run($set));
+
+    }
+
 }
