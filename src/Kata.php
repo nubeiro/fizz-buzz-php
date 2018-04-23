078df8ef10c5cf5ad91445469d6281f3aabb525a
Raúl Araya <nubeiro@gmail.com>
2018-04-23 23:07:30 +0200

    Refactor away mapWith, extract rules to apply.

--- src/Kata.php
+++ src/Kata.php
@@ -5,28 +5,27 @@ namespace Kata;
 
 class Kata
 {
-
     public function run(array $set = [])
     {
-        $ruleOfThree = function ($number) {
-            if ($number % 3 === 0) {
-                return 'Fizz';
-            }
-            return $number;
+        $isMultipleOfThree = function ($number) {
+            return (is_int($number) && $number % 3 === 0);
         };
-        $ruleOfFive = function ($number) {
-            if (is_int($number) && $number % 5 === 0) {
-                return 'Buzz';
-            }
-            return $number;
+        $isMultipleOFive = function($number) {
+            return (is_int($number) && $number % 5 === 0);
         };
-        $set = $this->mapWith($ruleOfThree, $set);
-        $set = $this->mapWith($ruleOfFive, $set);
+
+        $set = $this->apply($set, $isMultipleOfThree, 'Fizz');
+        $set = $this->apply($set, $isMultipleOFive, 'Buzz');
 
         return $set;
     }
 
-    public function mapWith(callable $rule, $set) {
-        return array_map($rule, $set);
+    public function apply($set, callable $rule, $fizzyBuzzy) {
+        return array_map(function ($number) use ($rule, $fizzyBuzzy) {
+            if ($rule($number)) {
+                return $fizzyBuzzy;
+            }
+            return $number;
+        }, $set);
     }
 }
